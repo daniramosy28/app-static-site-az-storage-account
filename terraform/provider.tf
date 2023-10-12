86e19a9da011eed6dcc4a8ece8fb8867a0d3c995
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-11 22:24:55 -0300

    será?

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,9 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id   = "subscriptionId"
-  tenant_id         = "tenantId"
-  client_id         = "clientId"
-  client_secret     = "clientSecret"
 }
